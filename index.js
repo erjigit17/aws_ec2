e0c847a7ff80954e2d588e9ff84ea966efe9b3ea
Эржигит Имамов <erjigit17@gmail.com>
2021-09-27 19:05:55 +0600

    port to 5000

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const http = require('http');
 
-
-
-
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.end('ok');
 });
 
-server.listen(8000);
+server.listen(5000);
 
 //https://ec2-3-127-22-131.eu-central-1.compute.amazonaws.com/
